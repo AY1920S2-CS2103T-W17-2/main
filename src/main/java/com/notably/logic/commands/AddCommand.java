a2ba7cec4eef51b147f3020c58c73b5e86b9094d
johannagwan <johannagwan@gmail.com>
2020-03-10 05:24:54 +0800

    Resolve merge conflicts

--- src/main/java/com/notably/logic/commands/AddCommand.java
+++ src/main/java/com/notably/logic/commands/AddCommand.java
@@ -9,6 +9,7 @@ import static java.util.Objects.requireNonNull;
 
 import com.notably.logic.commands.exceptions.CommandException;
 import com.notably.model.Model;
+import com.notably.model.person.Person;
 
 /**
  * Adds a person to the address book.
