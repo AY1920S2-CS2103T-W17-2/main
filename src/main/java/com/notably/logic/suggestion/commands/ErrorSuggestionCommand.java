637c4409f97adadb51cdae4581204a053d824a7c
johannagwan <johannagwan@gmail.com>
2020-03-17 15:13:50 +0800

    Add JavaDoc to ErrorSuggestionCommand class

--- src/main/java/com/notably/logic/suggestion/commands/ErrorSuggestionCommand.java
+++ src/main/java/com/notably/logic/suggestion/commands/ErrorSuggestionCommand.java
@@ -2,6 +2,9 @@ package com.notably.logic.suggestion.commands;
 
 import com.notably.model.Model;
 
+/**
+ * Represents an error command when the user input do not match any of the available commands or blocks.
+ */
 public class ErrorSuggestionCommand implements SuggestionCommand {
     @Override
     public void execute(Model model) {
