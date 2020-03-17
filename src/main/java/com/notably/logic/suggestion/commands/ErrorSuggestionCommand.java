a3f54953c88f5805250b16390e81f14ec49ff9a6
johannagwan <johannagwan@gmail.com>
2020-03-17 14:52:08 +0800

    Add ErrorSuggestionCommand class

---
+++ src/main/java/com/notably/logic/suggestion/commands/ErrorSuggestionCommand.java
@@ -0,0 +1,10 @@
+package com.notably.logic.suggestion.commands;
+
+import com.notably.model.Model;
+
+public class ErrorSuggestionCommand implements SuggestionCommand {
+    @Override
+    public void execute(Model model) {
+
+    }
+}
