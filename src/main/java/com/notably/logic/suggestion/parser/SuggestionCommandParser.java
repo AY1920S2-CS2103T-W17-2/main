6699cb81ac17bf0f7d693bb3ff4333731f6ee009
johannagwan <johannagwan@gmail.com>
2020-03-24 14:23:35 +0800

    Update parse method to take in model as param

--- src/main/java/com/notably/logic/suggestion/parser/SuggestionCommandParser.java
+++ src/main/java/com/notably/logic/suggestion/parser/SuggestionCommandParser.java
@@ -2,6 +2,7 @@ package com.notably.logic.suggestion.parser;
 
 import com.notably.logic.parser.exceptions.ParseException;
 import com.notably.logic.suggestion.SuggestionCommand;
+import com.notably.model.Model;
 
 /**
  * Represents a Suggestion Command Parser that is able to parse user input into a SuggestionCommand.
@@ -11,8 +12,9 @@ public interface SuggestionCommandParser<T extends SuggestionCommand> {
     /**
      * Parses user input into a suggestion command and returns it.
      * @param userInput The user input.
+     * @param model The app's model.
      * @return The suggestion command.
      * @throws ParseException if {@code userInput} does not conform to the expected format.
      */
-    T parse(String userInput) throws ParseException;
+    T parse(String userInput, Model model) throws ParseException;
 }
